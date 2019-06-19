ad36b15397dbebed80a41303b6bb0efac3f8dab0
shivamswarnkar <ss8464@nyu.edu>
2019-06-19 11:22:42 +0530

    add error handlers and error pages

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -29,11 +29,13 @@ def create_app(config_class=Config):
     from flaskblog.users.routes import users
     from flaskblog.posts.routes import posts
     from flaskblog.main.routes import main
+    from flaskblog.errors.handlers import errors
 
     # register blue prints
     app.register_blueprint(users)
     app.register_blueprint(posts)
     app.register_blueprint(main)
+    app.register_blueprint(errors)
 
     # return app
     return app

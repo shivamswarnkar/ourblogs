d97187b2a28f81df56865bf8a0f1b8e6659e869a
shivamswarnkar <ss8464@nyu.edu>
2019-07-16 21:27:00 +0530

    add policy routes and change name from flaskblog to ourblogs

--- flaskblog/main/routes.py
+++ flaskblog/main/routes.py
@@ -16,3 +16,11 @@ def home():
 @main.route('/about')
 def about():
     return render_template('about.html', title='About')
+
+@main.route('/privacy')
+def privacy():
+	return render_template('privacy.html', title='Privacy')
+
+@main.route('/terms')
+def terms():
+	return render_template('terms.html', title='Privacy')
